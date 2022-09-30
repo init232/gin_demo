1d63ca3fff76c13d08af50db57823ecc9c365f08
init <17521383564@163.com>
2022-09-30 15:53:38 +0800

    20220930

--- gin-form/main.go
+++ gin-form/main.go
@@ -1,12 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-//gin form表单
 func main() {
 	r := gin.Default()
+	r.LoadHTMLFiles("./login.html", "./index.html")
 	r.GET("/login", func(ctx *gin.Context) {
-		ctx.GetPostForm("")
+		ctx.HTML(http.StatusOK, "login.html", nil)
+	})
+	r.POST("/login", func(ctx *gin.Context) {
+		username := ctx.DefaultPostForm("username", "samebody")
+		password := ctx.DefaultPostForm("password", "")
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"Name": username,
+			"pass": password,
+		})
 	})
 	r.Run(":9090")
 }
